ca5ec2291e3c2f2c4189880cb21095851541a697
AliciaT <aliciaju@126.com>
2025-07-20 23:07:23 +0200

    prompt list

--- capp/admin.py
+++ capp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UsrSubmission, DialogHistory
 
-# Register your models here.
+@admin.register(UsrSubmission)
+class UsrSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'mode', 'abstract', 'temp', 'keepit', 'created_at')
+    search_fields = ('mode', 'usrinput')
+
+@admin.register(DialogHistory)
+class DialogHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at')
+    readonly_fields = ('prompt', 'usr', 'assistant')
