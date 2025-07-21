a12b9e24a79fcdcefdf4316554b0ec6bc72f9cfa
AliciaT <aliciaju@126.com>
2025-07-21 23:15:51 +0200

    分离static

--- BetterChat/settings.py
+++ BetterChat/settings.py
@@ -113,10 +113,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "capp" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
